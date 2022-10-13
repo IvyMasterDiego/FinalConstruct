bdee359e219c6067e4187735fe133a7f400346e5
IvyMasterDiego <diegolobato82@gmail.com>
2022-10-12 22:27:14 -0400

    Nuevo proyecto de encriptacion y guardado de informacion en un QR

---
+++ FinalProject/FinalProjectConstruct/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace FinalProjectConstruct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbQrCode = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCel = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.btnAddPhoto = new System.Windows.Forms.Button();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.lblQrCode = new System.Windows.Forms.Label();
+            this.pbPhotoface = new System.Windows.Forms.PictureBox();
+            this.lblPhotoFace = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPhotoface)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbQrCode
+            // 
+            this.pbQrCode.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbQrCode.Location = new System.Drawing.Point(12, 44);
+            this.pbQrCode.Name = "pbQrCode";
+            this.pbQrCode.Size = new System.Drawing.Size(245, 246);
+            this.pbQrCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbQrCode.TabIndex = 0;
+            this.pbQrCode.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 309);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 180);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(714, 280);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(633, 280);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(552, 280);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(471, 280);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblCedula
+            // 
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Location = new System.Drawing.Point(630, 15);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(40, 13);
+            this.lblCedula.TabIndex = 6;
+            this.lblCedula.Text = "Cedula";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(630, 70);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(44, 13);
+            this.lblLastName.TabIndex = 7;
+            this.lblLastName.Text = "Apellido";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(630, 44);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 13);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "Nombre";
+            // 
+            // lblCel
+            // 
+            this.lblCel.AutoSize = true;
+            this.lblCel.Location = new System.Drawing.Point(630, 96);
+            this.lblCel.Name = "lblCel";
+            this.lblCel.Size = new System.Drawing.Size(39, 13);
+            this.lblCel.TabIndex = 9;
+            this.lblCel.Text = "Celular";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(630, 122);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(52, 13);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "Direccion";
+            // 
+            // btnAddPhoto
+            // 
+            this.btnAddPhoto.Location = new System.Drawing.Point(390, 280);
+            this.btnAddPhoto.Name = "btnAddPhoto";
+            this.btnAddPhoto.Size = new System.Drawing.Size(75, 23);
+            this.btnAddPhoto.TabIndex = 11;
+            this.btnAddPhoto.Text = "Add Photo";
+            this.btnAddPhoto.UseVisualStyleBackColor = true;
+            this.btnAddPhoto.Click += new System.EventHandler(this.btnAddPhoto_Click);
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(688, 12);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 20);
+            this.txtCedula.TabIndex = 12;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(688, 41);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 13;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(688, 67);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 14;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(688, 93);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(100, 20);
+            this.txtCelular.TabIndex = 15;
+            // 
+            // lblQrCode
+            // 
+            this.lblQrCode.AutoSize = true;
+            this.lblQrCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQrCode.Location = new System.Drawing.Point(12, 10);
+            this.lblQrCode.Name = "lblQrCode";
+            this.lblQrCode.Size = new System.Drawing.Size(120, 31);
+            this.lblQrCode.TabIndex = 17;
+            this.lblQrCode.Text = "QRCode";
+            // 
+            // pbPhotoface
+            // 
+            this.pbPhotoface.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbPhotoface.Location = new System.Drawing.Point(471, 44);
+            this.pbPhotoface.Name = "pbPhotoface";
+            this.pbPhotoface.Size = new System.Drawing.Size(139, 148);
+            this.pbPhotoface.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbPhotoface.TabIndex = 18;
+            this.pbPhotoface.TabStop = false;
+            // 
+            // lblPhotoFace
+            // 
+            this.lblPhotoFace.AutoSize = true;
+            this.lblPhotoFace.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhotoFace.Location = new System.Drawing.Point(465, 9);
+            this.lblPhotoFace.Name = "lblPhotoFace";
+            this.lblPhotoFace.Size = new System.Drawing.Size(153, 31);
+            this.lblPhotoFace.TabIndex = 19;
+            this.lblPhotoFace.Text = "Face Photo";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(688, 122);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(100, 96);
+            this.txtDireccion.TabIndex = 20;
+            this.txtDireccion.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 499);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.lblPhotoFace);
+            this.Controls.Add(this.pbPhotoface);
+            this.Controls.Add(this.pbQrCode);
+            this.Controls.Add(this.lblQrCode);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.btnAddPhoto);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblCel);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblCedula);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPhotoface)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbQrCode;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblCedula;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCel;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Button btnAddPhoto;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.Label lblQrCode;
+        private System.Windows.Forms.PictureBox pbPhotoface;
+        private System.Windows.Forms.Label lblPhotoFace;
+        private System.Windows.Forms.RichTextBox txtDireccion;
+    }
+}
+
---
+++ FinalProject/FinalProjectConstruct/TextEncrypt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FinalProjectConstruct
+{
+    public static class TextEncrypt
+    {
+        public static byte[] IV { get; set; }
+        public static byte[] Key { get; set; }
+
+        public static string Encrypttext(this string str)
+        {
+            try
+            {
+                if (str == string.Empty)
+                    return string.Empty;
+                byte[] bytes = new UTF8Encoding().GetBytes(str);
+                TripleDESCryptoServiceProvider cryptoServiceProvider = new TripleDESCryptoServiceProvider();
+                cryptoServiceProvider.IV = TextEncrypt.IV;
+                cryptoServiceProvider.Key = TextEncrypt.Key;
+                ICryptoTransform encryptor = cryptoServiceProvider.CreateEncryptor(TextEncrypt.Key,
+                    TextEncrypt.IV);
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write);
+                cryptoStream.Write(bytes, 0, bytes.Length);
+                cryptoStream.FlushFinalBlock();
+                return Convert.ToBase64String(memoryStream.ToArray());
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+}

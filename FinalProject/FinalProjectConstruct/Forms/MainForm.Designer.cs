dce2957ae2f062984d6f99a3755ef5e95f2b131a
IvyMasterDiego <diegolobato82@gmail.com>
2022-10-16 04:40:19 -0400

    Se cambio el nombre del Formulario Principal

---
+++ FinalProject/FinalProjectConstruct/Forms/MainForm.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace FinalProjectConstruct
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbQrCode = new System.Windows.Forms.PictureBox();
+            this.dgvPerson = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCel = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtCedula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.TextBox();
+            this.lblQrCode = new System.Windows.Forms.Label();
+            this.txtDireccion = new System.Windows.Forms.RichTextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbQrCode
+            // 
+            this.pbQrCode.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbQrCode.Location = new System.Drawing.Point(12, 44);
+            this.pbQrCode.Name = "pbQrCode";
+            this.pbQrCode.Size = new System.Drawing.Size(245, 246);
+            this.pbQrCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbQrCode.TabIndex = 0;
+            this.pbQrCode.TabStop = false;
+            // 
+            // dgvPerson
+            // 
+            this.dgvPerson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPerson.Location = new System.Drawing.Point(12, 309);
+            this.dgvPerson.Name = "dgvPerson";
+            this.dgvPerson.Size = new System.Drawing.Size(650, 180);
+            this.dgvPerson.TabIndex = 1;
+            this.dgvPerson.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvPerson_RowHeaderMouseClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(506, 29);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(506, 58);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(547, 87);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(587, 58);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblCedula
+            // 
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Location = new System.Drawing.Point(342, 61);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(40, 13);
+            this.lblCedula.TabIndex = 6;
+            this.lblCedula.Text = "Cedula";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(342, 116);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(44, 13);
+            this.lblLastName.TabIndex = 7;
+            this.lblLastName.Text = "Apellido";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(342, 90);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 13);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "Nombre";
+            // 
+            // lblCel
+            // 
+            this.lblCel.AutoSize = true;
+            this.lblCel.Location = new System.Drawing.Point(342, 142);
+            this.lblCel.Name = "lblCel";
+            this.lblCel.Size = new System.Drawing.Size(39, 13);
+            this.lblCel.TabIndex = 9;
+            this.lblCel.Text = "Celular";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(342, 168);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(52, 13);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "Direccion";
+            // 
+            // txtCedula
+            // 
+            this.txtCedula.Location = new System.Drawing.Point(400, 58);
+            this.txtCedula.Name = "txtCedula";
+            this.txtCedula.Size = new System.Drawing.Size(100, 20);
+            this.txtCedula.TabIndex = 12;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(400, 87);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 13;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(400, 113);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 14;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.Location = new System.Drawing.Point(400, 139);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(100, 20);
+            this.txtCelular.TabIndex = 15;
+            // 
+            // lblQrCode
+            // 
+            this.lblQrCode.AutoSize = true;
+            this.lblQrCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQrCode.Location = new System.Drawing.Point(12, 10);
+            this.lblQrCode.Name = "lblQrCode";
+            this.lblQrCode.Size = new System.Drawing.Size(120, 31);
+            this.lblQrCode.TabIndex = 17;
+            this.lblQrCode.Text = "QRCode";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(400, 168);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(234, 96);
+            this.txtDireccion.TabIndex = 20;
+            this.txtDireccion.Text = "";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(587, 29);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 21;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(400, 32);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 23;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(342, 35);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(18, 13);
+            this.lblId.TabIndex = 22;
+            this.lblId.Text = "ID";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 499);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.pbQrCode);
+            this.Controls.Add(this.lblQrCode);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCedula);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblCel);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblCedula);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dgvPerson);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QRCodeGenerator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbQrCode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPerson)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbQrCode;
+        private System.Windows.Forms.DataGridView dgvPerson;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblCedula;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCel;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtCedula;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtCelular;
+        private System.Windows.Forms.Label lblQrCode;
+        private System.Windows.Forms.RichTextBox txtDireccion;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblId;
+    }
+}
+
---
+++ FinalProject/FinalProjectConstruct/Forms/MainForm.cs
@@ -0,0 +1,162 @@
+﻿using FinalProjectConstruct.Class.Model;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity.Core.Mapping;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace FinalProjectConstruct
+{
+    public partial class MainForm : Form
+    {
+        string txtecnrypt;
+        PersonInformation person = new PersonInformation();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            dgvPerson.DataSource = person.GetPersons();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            TextEncrypt.IV = Encoding.ASCII.GetBytes("dkr*vcd@");
+            TextEncrypt.Key = Encoding.ASCII.GetBytes("regshajshsdrkrmwcbsgskfe");
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtCedula.Text) && string.IsNullOrEmpty(txtNombre.Text) &&
+                string.IsNullOrEmpty(txtApellido.Text) && string.IsNullOrEmpty(txtCelular.Text) &&
+                string.IsNullOrEmpty(txtDireccion.Text))
+            {
+                MessageBox.Show("Debe completar los campos antes de generar el QR", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                txtecnrypt = this.EncryptText();
+                QRCodeGenerator qRCode = new QRCodeGenerator();
+                ASCIIEncoding encoding = new ASCIIEncoding();
+
+                var createcode = qRCode.CreateQrCode(encoding.GetBytes(txtecnrypt), QRCodeGenerator.ECCLevel.L);
+                PngByteQRCode png = new PngByteQRCode();
+                png.SetQRCodeData(createcode);
+                var arr = png.GetGraphic(10);
+                MemoryStream memoryStream = new MemoryStream();
+                memoryStream.Write(arr, 0, arr.Length);
+                Bitmap bitmap = new Bitmap(memoryStream);
+                pbQrCode.Image = bitmap;
+            }
+        }
+
+        private string EncryptText()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(txtCedula.Text + "||");
+            sb.Append(txtNombre.Text + "||");
+            sb.Append(txtApellido.Text + "||");
+            sb.Append(txtCelular.Text + "||");
+            sb.Append(txtDireccion.Text + "||");
+
+            return sb.ToString().Encrypttext() + "###";
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            person.Cedula = txtCedula.Text;
+            person.Nombre = txtNombre.Text;
+            person.Apellido = txtApellido.Text;
+            person.Celular = txtCelular.Text;
+            person.Direccion = txtDireccion.Text;
+
+            var success = person.InsertPerson(person);
+            dgvPerson.DataSource = person.GetPersons();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Agregado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error. Intente otra vez");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            person.Id = int.Parse(txtId.Text);
+            person.Cedula = txtCedula.Text;
+            person.Nombre = txtNombre.Text;
+            person.Apellido = txtApellido.Text;
+            person.Celular = txtCelular.Text;
+            person.Direccion = txtDireccion.Text;
+
+            var success = person.UpdatePerson(person);
+            dgvPerson.DataSource = person.GetPersons();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Actualizado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error. Intente otra vez");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            person.Cedula = txtCedula.Text;
+            var success = person.DeletePerson(person);
+            dgvPerson.DataSource = person.GetPersons();
+            if (success)
+            {
+                ClearControls();
+                MessageBox.Show("Actualizado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error. Intente otra vez");
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearControls();
+        }
+
+        private void ClearControls()
+        {
+            txtId.Text = String.Empty;
+            txtCedula.Text = String.Empty;
+            txtNombre.Text = String.Empty;
+            txtApellido.Text = String.Empty;
+            txtCelular.Text = String.Empty;
+            txtDireccion.Text = String.Empty;
+        }
+
+        private void dgvPerson_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            var index = e.RowIndex;
+            txtId.Text = dgvPerson.Rows[index].Cells[0].Value.ToString();
+            txtCedula.Text = dgvPerson.Rows[index].Cells[1].Value.ToString();
+            txtNombre.Text = dgvPerson.Rows[index].Cells[2].Value.ToString();
+            txtApellido.Text = dgvPerson.Rows[index].Cells[3].Value.ToString();
+            txtCelular.Text = dgvPerson.Rows[index].Cells[4].Value.ToString();
+            txtDireccion.Text = dgvPerson.Rows[index].Cells[5].Value.ToString();
+        }
+    }
+}
